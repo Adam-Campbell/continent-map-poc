1ec7fda079a84b18a19d99d4733f5006b676482d
Adam Campbell <adamcampbelldev@gmail.com>
2024-12-09 16:17:31 +0000

    refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,9 @@
  * https://www.georgethegeographer.co.uk/Base_maps/World_7colour_unnamed.jpg
  */
 
-console.log("Hello from script.js");
-
-const canvas = document.getElementById("continent-canvas");
-const ctx = canvas.getContext("2d");
-const wrapper = document.getElementById("wrapper");
+/**
+ * rgb colors mapped to continents
+ */
 const colorsContinents = {
     "rgb(255, 204, 0)": "North America",
     "rgb(51, 204, 51)": "South America",
@@ -18,47 +16,66 @@ const colorsContinents = {
     "rgb(0, 153, 255)": "Antarctica"
 }
 
+/**
+ * References to elements
+ */
+const canvas = document.getElementById("continent-canvas");
+const ctx = canvas.getContext("2d");
+const wrapper = document.getElementById("wrapper");
+
+/**
+ * Global variables to hold images natural dimensions
+ */
 let intrinsicWidth = null;
 let intrinsicHeight = null;
 
-
+/**
+ * Load image into memory, store natural dimensions and render canvas
+ */
 const img = new Image();
 img.src = "assets/images/continents-map.jpg";
 img.onload = () => {
     intrinsicWidth = img.width;
     intrinsicHeight = img.height;
-    const width = intrinsicWidth * 0.5;
-    const height = intrinsicHeight * 0.5;
-    canvas.width = width;
-    canvas.height = height;
-    ctx.drawImage(img, 0, 0, width, height);
+    renderCanvas();
 };
 
+/**
+ * Render image to canvas, with size calculated based on width of wrapper div 
+ */
+function renderCanvas() {
+    const wrapperWidth = wrapper.getBoundingClientRect().width;
+    // Calculate scale factor for transforming intrinsic dimensions to rendered dimensions.
+    // Ensure that maximum scale factor is 1 to prevent image being rendered larger than its natural size.
+    const scaleFactor = Math.min(wrapperWidth / intrinsicWidth, 1);
+    // Calculate rendered dimensions
+    const renderedWidth = intrinsicWidth * scaleFactor;
+    const renderedHeight = intrinsicHeight * scaleFactor;
+    // Set canvas dimensions and draw image
+    canvas.width = renderedWidth;
+    canvas.height = renderedHeight;
+    ctx.drawImage(img, 0, 0, renderedWidth, renderedHeight);
+}
+
+/**
+ * Add event listener to canvas that retrieves the pixel color of the clicked location, and if that
+ * color corresponds to a continent, show an alert with the name of the continent.
+ */
 canvas.addEventListener("click", e => {
     const x = e.offsetX;
     const y = e.offsetY;
     const pixel = ctx.getImageData(x, y, 1, 1).data;
     const rgbString = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
-
+    // Only show alert if the color matches a continent
     if (colorsContinents[rgbString]) {
         const continentClicked = colorsContinents[rgbString];
         alert(`You clicked on ${continentClicked}`);
     }
 
-    //console.log(rgb);
 });
 
-function handleResize(e) {
-    const wrapperWidth = wrapper.getBoundingClientRect().width;
-    const scaleFactor = Math.min(wrapperWidth / intrinsicWidth, 1);
-    const renderedWidth = intrinsicWidth * scaleFactor;
-    const renderedHeight = intrinsicHeight * scaleFactor;
-    canvas.width = renderedWidth;
-    canvas.height = renderedHeight;
-    ctx.drawImage(img, 0, 0, renderedWidth, renderedHeight);
 
-}
-
-window.addEventListener("resize", handleResize);
+// Add event listener to window that re-renders the canvas when the window is resized
+window.addEventListener("resize", renderCanvas);
 
 
